517165708f102006a45b0a335282743a739256f1
knowncolor <github@knowncolor.com>
2015-04-14 17:58:59 +0100

    Updated stylesheet and added sample post-example.html page.
    Added header styles up to h6.
    Removed in-lining of font stylesheets as they caused fonts not to load
    on mobile.
    Added a post-example.html file to show how a post/article might look
    without requiring local stack.

--- partials/header.php
+++ partials/header.php
@@ -5,6 +5,7 @@
 		<meta charset="utf-8">
 		<meta name="description" content="<?php echo site_description(); ?>">
 		<link rel="stylesheet" href="<?php echo theme_url('assets/style.css'); ?>" media="screen" type="text/css">
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Bitter:400,700|Droid+Sans+Mono"  type="text/css">
 		<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php echo base_url(); ?>/feeds/rss">
 	</head>
 	<body>
